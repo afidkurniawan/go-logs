f7614bbf33577f29c7406995ff7a92f202882919
Mohammad Badrullami <mohammad.badrullami@gmail.com>
2021-01-15 22:52:44 +0700

    add translation wizard top

--- src/app/components/Wizard/Top.tsx
+++ src/app/components/Wizard/Top.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { translations } from "../../../locales/i18n";
-import { topWizard } from "../../../lib/Constants";
 
 import { Title, Row, Column, Item, ItemTitle } from "../../../styles/Wizard";
 import {
@@ -19,19 +18,63 @@ export default function Top() {
     <Container>
       <Title>{t(translations.wizard.top.title)}</Title>
       <Row>
-        {topWizard.map((key, i) => (
-          <Column key={i}>
-            <Icon src={key.link} />
-            <Item>
-              <Rectangle show={key.prev} />
-              <Circle active={key.active}>
-                <Number active={key.active}>{i + 1}</Number>
-              </Circle>
-              <Rectangle show={key.next} />
-            </Item>
-            <ItemTitle>{key.title}</ItemTitle>
-          </Column>
-        ))}
+        <Column>
+          <Icon src={require("../../../assets/icons/iwizard1.svg")} />
+          <Item>
+            <Rectangle show={false} />
+            <Circle active={true}>
+              <Number active={true}>1</Number>
+            </Circle>
+            <Rectangle show={true} />
+          </Item>
+          <ItemTitle>{t(translations.wizard.top.requestForm)}</ItemTitle>
+        </Column>
+        <Column>
+          <Icon src={require("../../../assets/icons/iwizard2.svg")} />
+          <Item>
+            <Rectangle show={true} />
+            <Circle active={false}>
+              <Number active={false}>2</Number>
+            </Circle>
+            <Rectangle show={true} />
+          </Item>
+          <ItemTitle>{t(translations.wizard.top.confirmation)}</ItemTitle>
+        </Column>
+        <Column>
+          <Icon src={require("../../../assets/icons/iwizard3.svg")} />
+          <Item>
+            <Rectangle show={true} />
+            <Circle active={false}>
+              <Number active={false}>3</Number>
+            </Circle>
+            <Rectangle show={true} />
+          </Item>
+          <ItemTitle>{t(translations.wizard.top.proformaInvoice)}</ItemTitle>
+        </Column>
+        <Column>
+          <Icon src={require("../../../assets/icons/iwizard4.svg")} />
+          <Item>
+            <Rectangle show={true} />
+            <Circle active={false}>
+              <Number active={false}>4</Number>
+            </Circle>
+            <Rectangle show={true} />
+          </Item>
+          <ItemTitle>
+            {t(translations.wizard.top.paymentConfirmation)}
+          </ItemTitle>
+        </Column>
+        <Column>
+          <Icon src={require("../../../assets/icons/iwizard5.svg")} />
+          <Item>
+            <Rectangle show={true} />
+            <Circle active={false}>
+              <Number active={false}>5</Number>
+            </Circle>
+            <Rectangle show={false} />
+          </Item>
+          <ItemTitle>{t(translations.wizard.top.doRelease)}</ItemTitle>
+        </Column>
       </Row>
     </Container>
   );
