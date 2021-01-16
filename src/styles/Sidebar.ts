51b90060aeb54d389a77603f103a3800f46880ca
Mohammad Badrullami <mohammad.badrullami@gmail.com>
2021-01-16 22:04:22 +0700

    responsive stylesheet sidebar

--- src/styles/Sidebar.ts
+++ src/styles/Sidebar.ts
@@ -2,21 +2,20 @@ import styled from "styled-components/macro";
 
 export const Container = styled.div`
   position: fixed;
-  width: 280px;
-  height: 800px;
-  left: 16px;
-  top: 16px;
+  width: 17%;
+  height: 95vh;
+  left: 1rem;
+  top: 1.5rem;
   background: #ffffff;
   box-shadow: 4px 4px 24px rgba(183, 182, 255, 0.2);
-  border-radius: 20px;
+  border-radius: 1rem;
 `;
 
 export const Logo = styled.div`
   position: absolute;
-  width: 225px;
-  height: 109px;
-  left: 28px;
-  top: 0px;
+  width: 100%;
+  height: 100%;
+  padding-left: 3rem;
   cursor: pointer;
 `;
 
